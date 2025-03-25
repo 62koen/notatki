cee4464ad21474bfc613273e9d29b07fb20d547f
62koen <ejeqyj57@addy.to>
2025-03-25 18:19:53 +0100

    Quartz sync: Mar 25, 2025, 6:19 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,6 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       Kod: "https://github.com/62koen/notatki",
       "Wygląd inspirowany aplikacją Obsidian": "https://obsidian.md",
+      "Każda notatka, jeśli nie napisano inaczej, jest udostępniona w domenie publicznej na zasadach CC0 1.0": "https://creativecommons.org/publicdomain/zero/1.0/deed.pl"
     },
   }),
 }
